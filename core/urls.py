73fb709cf69522d6f2965e41d94b5443bfad79c1
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-16 13:02:29 -0300

    Listagem Veiculos e MovRotativo

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 
-from .views import home, lista_pessoas
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos
+    )
 
 app_name = "core"
 urlpatterns = [
     path("", home, name="home"),
-    path("pessoas/", lista_pessoas, name="core_lista_pessoas")
+    path("pessoas/", lista_pessoas, name="core_lista_pessoas"),
+    path("veiculos/", lista_veiculos, name="core_lista_veiculos"),
+    path("mov-rot-list/", lista_movrotativos, name="core_lista_movrotativos")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovRotativo
+    )
 
 
 def home(request):
@@ -10,3 +14,14 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, "core/lista_pessoas.html", {"pessoas": pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, "core/lista_veiculos.html", {"veiculos": veiculos})
+
+
+def lista_movrotativos(request):
+    mov_rot = MovRotativo.objects.all()
+    return render(
+        request, "core/lista_movrotativos.html", {"mov_rot": mov_rot})
