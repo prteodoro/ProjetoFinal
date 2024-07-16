2170f09d0617e93505fe0471289f158a07e55d46
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-15 21:46:25 -0300

    Criado classes Pessoa/Marca/Veiculo

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Marca, Veiculo, Pessoa
 
-# Register your models here.
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
