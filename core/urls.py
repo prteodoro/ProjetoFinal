5f5bf4a7ab79ed4e800b7eb419f6567fbdf5f35e
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-16 00:53:10 -0300

    Listagem Pessoa

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import home
+from .views import home, lista_pessoas
 
 app_name = "core"
 urlpatterns = [
     path("", home, name="home"),
+    path("pessoas/", lista_pessoas, name="core_lista_pessoas")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 
 def home(request):
     context = {"mensagem": "Ola mundo"}
     return render(request, "base.html", context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, "core/lista_pessoas.html", {"pessoas": pessoas})
