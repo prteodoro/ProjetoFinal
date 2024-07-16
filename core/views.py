77255ef266c0da87521ef9e13470508be0afde60
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-16 14:09:33 -0300

    Listagem Mensalistas e MovMensalistas

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import (
     Pessoa,
     Veiculo,
-    MovRotativo
+    MovRotativo,
+    Mensalista,
+    MovMensalista
     )
 
 
@@ -13,15 +15,26 @@ def home(request):
 
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
-    return render(request, "core/lista_pessoas.html", {"pessoas": pessoas})
+    return render(request, "core/lista_pessoas.html", {"pessoas": pessoas })
 
 
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
-    return render(request, "core/lista_veiculos.html", {"veiculos": veiculos})
+    return render(request, "core/lista_veiculos.html", {"veiculos": veiculos })
 
 
 def lista_movrotativos(request):
     mov_rot = MovRotativo.objects.all()
     return render(
-        request, "core/lista_movrotativos.html", {"mov_rot": mov_rot})
+        request, "core/lista_movrotativos.html", {"mov_rot": mov_rot })
+
+
+def lista_mensalistas(request):
+    mensalistas = Mensalista.objects.all()
+    return render(
+        request, "core/lista_mensalistas.html", { "mensalistas": mensalistas })
+
+
+def lista_movmensalistas(request):
+    mov_mensalistas = MovMensalista.objects.all()
+    return render(request, "core/lista_movmensalistas.html", { "mov_mensalistas": mov_mensalistas})
