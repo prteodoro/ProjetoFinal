00981d3c4574ae405787e3068c555ee32f863c11
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-20 21:11:37 -0300

    Templates Bootstrap

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -33,14 +33,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.messages',    
+    'django.contrib.staticfiles',    
     "core",
+    "bootstrapform",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
