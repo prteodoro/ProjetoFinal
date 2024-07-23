dcde070353c66fea6f2d36c342ba7558494c73f1
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-23 11:40:08 -0300

    Criando app website

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', include('website.urls')),
+    path("sistema/", include("core.urls")),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
