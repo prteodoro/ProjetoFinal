35a05bba86d616b6f4d87af810fefb05a2f2fb3a
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-25 20:31:49 -0300

    Salvando contato no banco de dados

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.nome
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contato
 
 
 def home(request):
@@ -6,7 +7,21 @@ def home(request):
 
 
 def contato(request):
-    return render(request, "website/contato.html")
+    mensagem = ""
+    if request.method == "POST":
+        try:
+            contato = {}
+            contato["nome"] = request.POST.get("nome")
+            contato["email"] = request.POST.get("email")
+            contato["telefone"] = request.POST.get("telefone")
+            contato["mensagem"] = request.POST.get("mensagem")
+
+            Contato.objects.create(**contato)
+        except Exception as e:
+            mensagem = str(e)
+        else:
+            mensagem = "contato realizado com sucesso"
+    return render(request, "website/contato.html", {"mensagem": mensagem})
 
 
 def servicos(request):
